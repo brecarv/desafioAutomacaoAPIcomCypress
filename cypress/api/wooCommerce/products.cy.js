9b0c61671e88f44ead4dc2801cf1e3e4ab3da064
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-11-21 15:21:51 -0300

    feat: create a product - acceptance and contract  On branch feat/product  Your branch is ahead of 'origin/feat/product' by 1 commit.  Changes to be committed: 	modified:   cypress/api/wooCommerce/products.cy.js 	modified:   cypress/support/requests/wooCommerce/products.js

--- cypress/api/wooCommerce/products.cy.js
+++ cypress/api/wooCommerce/products.cy.js
@@ -6,6 +6,19 @@ import tokenFixture from "../../fixtures/token.json";
 import productsSchema from "../../contracts/products.contract";
 
 describe("Products", () => {
+  let productName = faker.commerce.product();
+  let productType = "simple";
+  let productPrice = faker.commerce.price();
+  let productDescription = faker.commerce.productDescription();
+  console.log("antes:" + productDescription);
+  let productShortDescription = productDescription.substring(
+    1,
+    productDescription.length / 2
+  );
+  let categoryID = 48;
+  let productCategories = [{ id: categoryID }];
+  console.log("depois:" + productDescription);
+
   it("List all products - Acceptance", () => {
     cy.getProducts(tokenFixture.token).then((res) => {
       expect(res.status).to.eq(StatusCodes.OK);
@@ -34,6 +47,40 @@ describe("Products", () => {
   it("Retrieve a product - Contract", () => {
     let productID = 385;
     cy.getProductsByID(tokenFixture.token, productID).then((res) => {
+      return productsSchema.validateAsync(res.body);
+    });
+  });
+
+  it("Create a product - Acceptance", () => {
+    cy.postProducts(
+      tokenFixture.token,
+      productName,
+      productType,
+      productPrice,
+      productDescription,
+      productShortDescription,
+      productCategories
+    ).then((res) => {
+      expect(res.status).to.eq(StatusCodes.CREATED);
+      expect(res.body.name).to.eq(productName);
+      expect(res.body.type).to.eq(productType);
+      expect(res.body.regular_price).to.eq(productPrice);
+      expect(res.body.description).to.include(productDescription);
+      expect(res.body.short_description).to.eq(productShortDescription);
+      expect(res.body.categories[0]).to.have.any.property("id");
+    });
+  });
+
+  it("Create a product - Contract", () => {
+    cy.postProducts(
+      tokenFixture.token,
+      productName,
+      productType,
+      productPrice,
+      productDescription,
+      productShortDescription,
+      productCategories
+    ).then((res) => {
       return productsSchema.validateAsync(res.body);
     });
   });
--- cypress/support/requests/wooCommerce/products.js
+++ cypress/support/requests/wooCommerce/products.js
@@ -17,3 +17,32 @@ Cypress.Commands.add("getProductsByID", (token, id) => {
     },
   });
 });
+
+Cypress.Commands.add(
+  "postProducts",
+  (
+    token,
+    name,
+    type,
+    regular_price,
+    description,
+    short_description,
+    categories
+  ) => {
+    cy.request({
+      method: "POST",
+      url: Cypress.env("wooCommerce") + Cypress.env("products"),
+      headers: {
+        Authorization: token,
+      },
+      body: {
+        name: name,
+        type: type,
+        regular_price: regular_price,
+        description: description,
+        short_description: short_description,
+        categories: categories,
+      },
+    });
+  }
+);
