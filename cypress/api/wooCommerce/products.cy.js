f43d7c4fb018092f19cd8fd54855339a4da456f0
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-11-22 11:55:03 -0300

    refactor: list/retrieve/create/delete - acceptance and contract merged  On branch feat/product  Your branch is ahead of 'origin/feat/product' by 2 commits.  Changes to be committed: 	modified:   cypress/api/wooCommerce/products.cy.js

--- cypress/api/wooCommerce/products.cy.js
+++ cypress/api/wooCommerce/products.cy.js
@@ -10,48 +10,24 @@ describe("Products", () => {
   let productType = "simple";
   let productPrice = faker.commerce.price();
   let productDescription = faker.commerce.productDescription();
-  console.log("antes:" + productDescription);
   let productShortDescription = productDescription.substring(
     1,
     productDescription.length / 2
   );
   let categoryID = 48;
   let productCategories = [{ id: categoryID }];
-  console.log("depois:" + productDescription);
 
-  it("List all products - Acceptance", () => {
+  it("List all products - Acceptance and Contract", () => {
     cy.getProducts(tokenFixture.token).then((res) => {
       expect(res.status).to.eq(StatusCodes.OK);
       expect(res.body).to.have.length.greaterThan(0);
-    });
-  });
-
-  it("List all products - Contract", () => {
-    cy.getProducts(tokenFixture.token).then((res) => {
       for (let i = 0; i < res.body.length; i++) {
         return productsSchema.validateAsync(res.body[i]);
       }
     });
   });
 
-  it("Retrieve a product - Acceptance", () => {
-    let productID = 385;
-    cy.getProductsByID(tokenFixture.token, productID).then((res) => {
-      expect(res.status).to.eq(StatusCodes.OK);
-      expect(res.body.id).to.eq(productID);
-      expect(res.body.name).to.eq("Xiaomi Redmi 4X");
-      expect(res.body.type).to.eq("simple");
-    });
-  });
-
-  it("Retrieve a product - Contract", () => {
-    let productID = 385;
-    cy.getProductsByID(tokenFixture.token, productID).then((res) => {
-      return productsSchema.validateAsync(res.body);
-    });
-  });
-
-  it("Create a product - Acceptance", () => {
+  it("Retrieve a product - Acceptance and Contract", () => {
     cy.postProducts(
       tokenFixture.token,
       productName,
@@ -61,6 +37,37 @@ describe("Products", () => {
       productShortDescription,
       productCategories
     ).then((res) => {
+      let productID = res.body.id;
+      let force = true;
+      cy.getProductsByID(tokenFixture.token, productID).then((res) => {
+        expect(res.status).to.eq(StatusCodes.OK);
+        expect(res.body.id).to.eq(productID);
+        expect(res.body.name).to.eq(productName);
+        expect(res.body.type).to.eq(productType);
+        expect(res.body.regular_price).to.eq(productPrice);
+        expect(res.body.description).to.include(productDescription);
+        expect(res.body.short_description).to.include(productShortDescription);
+        expect(res.body.categories[0]).to.have.any.property("id");
+        return (
+          productsSchema.validateAsync(res.body),
+          cy.deleteProductsByID(tokenFixture.token, productID, force)
+        );
+      });
+    });
+  });
+
+  it("Create a product - Acceptance and Contract", () => {
+    cy.postProducts(
+      tokenFixture.token,
+      productName,
+      productType,
+      productPrice,
+      productDescription,
+      productShortDescription,
+      productCategories
+    ).then((res) => {
+      let productID = res.body.id;
+      let force = true;
       expect(res.status).to.eq(StatusCodes.CREATED);
       expect(res.body.name).to.eq(productName);
       expect(res.body.type).to.eq(productType);
@@ -68,10 +75,14 @@ describe("Products", () => {
       expect(res.body.description).to.include(productDescription);
       expect(res.body.short_description).to.eq(productShortDescription);
       expect(res.body.categories[0]).to.have.any.property("id");
+      return (
+        productsSchema.validateAsync(res.body),
+        cy.deleteProductsByID(tokenFixture.token, productID, force)
+      );
     });
   });
 
-  it("Create a product - Contract", () => {
+  it("Update a product - Acceptance and Contract", () => {
     cy.postProducts(
       tokenFixture.token,
       productName,
@@ -81,30 +92,23 @@ describe("Products", () => {
       productShortDescription,
       productCategories
     ).then((res) => {
-      return productsSchema.validateAsync(res.body);
+      let productID = res.body.id;
+      let newProductPrice = Number(productPrice / 2).toString();
+      let force = true;
+      cy.putProductsByID(tokenFixture.token, productID, newProductPrice).then(
+        (res) => {
+          expect(res.status).to.eq(StatusCodes.OK);
+          expect(res.body.price).to.eq(newProductPrice);
+          return (
+            productsSchema.validateAsync(res.body),
+            cy.deleteProductsByID(tokenFixture.token, productID, force)
+          );
+        }
+      );
     });
   });
 
-  it("Update a product - Acceptance", () => {
-    let productID = 5117;
-    cy.putProductsByID(tokenFixture.token, productID, productPrice).then(
-      (res) => {
-        expect(res.status).to.eq(StatusCodes.OK);
-        expect(res.body.price).to.eq(productPrice);
-      }
-    );
-  });
-
-  it("Update a product - Contract", () => {
-    let productID = 5117;
-    cy.putProductsByID(tokenFixture.token, productID, productPrice).then(
-      (res) => {
-        return productsSchema.validateAsync(res.body);
-      }
-    );
-  });
-
-  it.only("Delete a product - Acceptance and Contract", () => {
+  it("Delete a product - Acceptance and Contract", () => {
     cy.postProducts(
       tokenFixture.token,
       productName,
